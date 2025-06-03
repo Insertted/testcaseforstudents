b4679f1d5b4d92e6ceb657b933d5e949f564b7b1
Insert <seregasmolnyi48@gmail.com>
2025-06-03 08:31:26 +0000

    new file:   .env.example 	modified:   app/repository/database.py 	modified:   docker-compose.yml 	modified:   main.py

--- app/repository/database.py
+++ app/repository/database.py
@@ -7,8 +7,35 @@ conn = psycopg2.connect(user=DB_CONFIG['user'],
                         host=DB_CONFIG['host'],
                         dbname=DB_CONFIG['dbname'])
 
-cur = conn.cursor()
+class BaseRepo():
+    def __get_cursor(cls):
+         return conn.cursor()
+    def ping_tables(cls):
+        #cur = cls.__get_cursor()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM cats;")
+        print(cur.fetchall())
+    
+    def get_cats():
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM cats;")
+        print(cur.fetchall())
 
-cur.execute("SELECT * FROM students")
-records = cur.fetchall()   
-print(records)
+
+    def create_cat(name, color, tail_length, whiskers_length):
+        cur = conn.cursor()
+        cur.execute("INSERT INTO cats(name, color, tail_length, whiskers_length) VALUES (%s%s%s%s)", (name, color, tail_length, whiskers_length))
+
+
+    def delete_cat(name):
+        cur = conn.cursor()
+        cur.execute("DELETE FROM cats WHERE name=(%s)", (name))
+
+
+    def update_cat(name):
+        cur = conn.cursor()
+        cur.execute("UPDATE cats SET tail_length = '123' WHERE name = (%s)", (name))
+
+baseRepo = BaseRepo()
+#baseRepo.ping_tables()
+baseRepo.create_cat(name="Musya", color="ginger", tail_length=100, whiskers_length=5)
